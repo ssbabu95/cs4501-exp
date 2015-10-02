b13cdde139e97f1b0e5ac6e6db7585d834603d3a
ssbabu95 <babu.sumeet1@gmail.com>
2015-10-01 22:41:13 -0400

    Simple homepage logic - most recent listing

---
+++ cs4501/main.py
@@ -0,0 +1,15 @@
+import urllib.request
+import urllib.parse
+import json
+from django.http import JsonResponse
+
+def homepage(request):
+
+	req = urllib.request.Request('http://10.0.2.2:8001/api/v1/listing/recent')
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	return JsonResponse(resp)
+
+def listing_det(request, listing_id):
+
+
