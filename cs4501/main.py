2308e8d198b58ac0eaf251b146661bc8eb1225f0
ssbabu95 <babu.sumeet1@gmail.com>
2015-10-04 19:42:16 -0400

    Project 3

--- cs4501/main.py
+++ cs4501/main.py
@@ -11,5 +11,8 @@ def homepage(request):
 	return JsonResponse(resp)
 
 def listing_det(request, listing_id):
-
+	req = urllib.request.Request('http://10.0.2.2:8001/api/v1/listing/' + listing_id)
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp = json.loads(resp_json)
+	return JsonResponse(resp)
 
